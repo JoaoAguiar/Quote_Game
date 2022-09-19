bc9856c95d761ab1ec21e2d1fa45de19591d770b
JoaoAguiar <jpaguiar1998@gmail.com>
2022-09-19 17:17:18 +0100

    Commit1

---
+++ README.md
@@ -0,0 +1,4 @@
+# Quote_Game
+> https://joaoaguiar.github.io/Quote_Game/
+ 
+Small quiz game created with the intention of improve web skills
---
+++ game.js
@@ -0,0 +1,404 @@
+const questions = [
+    {
+        question: "'For what shall it profit a man, if he gain the whole world, and suffer the loss of his soul?'",
+        optionA: "Jesus Christ",
+        optionB: "Gustavo Santos",
+        correctOption: "optionA"
+    },
+    {
+        question: "'The present, if we divide the word in two, is a pre-feel. It is before feeling'",
+        optionA: "Jesus Christ",
+        optionB: "Gustavo Santos",
+        correctOption: "optionB"
+    },
+    {
+        question: "'And know that I am with you always; yes, to the end of time'",
+        optionA: "Jesus Christ",
+        optionB: "Gustavo Santos",
+        correctOption: "optionA"
+    },
+    {
+        question: "'The love of my life is me.'",
+        optionA: "Jesus Christ",
+        optionB: "Gustavo Santos",
+        correctOption: "optionB"
+    },
+    {
+        question: "'So I say to you, Ask and it will be given to you; search, and you will find; knock, and the door will be opened for you'",
+        optionA: "Jesus Christ",
+        optionB: "Gustavo Santos",
+        correctOption: "optionA"
+    },
+    {
+        question: "'There are 3 times, past, present and future, right? Then there is now. The now is not the present, I come to say that in my 5th book called 'Grab the Now''",
+        optionA: "Jesus Christ",
+        optionB: "Gustavo Santos",
+        correctOption: "optionB"
+    },
+    {
+        question: "'But I say to you, Love your enemies and pray for those who persecute you, so that you may be sons of your Father'",
+        optionA: "Jesus Christ",
+        optionB: "Gustavo Santos",
+        correctOption: "optionA"
+    },
+    {
+        question: "'We cannot feel and think at the same time. Either you think or feel'",
+        optionA: "Jesus Christ",
+        optionB: "Gustavo Santos",
+        correctOption: "optionB"
+    },
+    {
+        question: "'Give to everyone who begs from you; and of him who takes away your goods do not ask them again. And as you wish that men would do to you, do so to them.'",
+        optionA: "Jesus Christ",
+        optionB: "Gustavo Santos",
+        correctOption: "optionA"
+    },
+    {
+        question: "'If you are kissing someone, in principle you will be feeling that emotion, but if you are kissing someone and your mind takes you to what you have to do tomorrow, your tongue walks around but you don't feel anything'",
+        optionA: "Jesus Christ",
+        optionB: "Gustavo Santos",
+        correctOption: "optionB"
+    },
+    {
+        question: "'Behold, I stand at the door and knock. If anyone hears my voice and opens the door, I will come in to him and eat with him, and he with me'",
+        optionA: "Jesus Christ",
+        optionB: "Gustavo Santos",
+        correctOption: "optionA"
+    },
+    {
+        question: "'Our mind is called mind because it lies to us every day'",
+        optionA: "Jesus Christ",
+        optionB: "Gustavo Santos",
+        correctOption: "optionB"
+    },
+    {
+        question: "'It is easier for a camel to go through the eye of a needle than for a rich man to enter the kingdom'",
+        optionA: "Jesus Christ",
+        optionB: "Gustavo Santos",
+        correctOption: "optionA"
+    },
+    {
+        question: "'People have expectations, certainties, that that will happen. Result, nothing happens, you get beaten and they deserve it'",
+        optionA: "Jesus Christ",
+        optionB: "Gustavo Santos",
+        correctOption: "optionB"
+    },
+    {
+        question: "'And, behold, I come quickly; and my reward is with Me, to give every man according as his work shall be. I am Alpha and Omega, the beginning and the end, the first and the last.'",
+        optionA: "Jesus Christ",
+        optionB: "Gustavo Santos",
+        correctOption: "optionA"
+    },
+    {
+        question: "'Living attached to the past is like breathing through a fan, it's not you who do it, it's something or someone who does it for you'",
+        optionA: "Jesus Christ",
+        optionB: "Gustavo Santos",
+        correctOption: "optionB"
+    },
+    {
+        question: "'Do not be anxious about tomorrow, for tomorrow will be anxious for itself. Let the day's own trouble be sufficient for the day'",
+        optionA: "Jesus Christ",
+        optionB: "Gustavo Santos",
+        correctOption: "optionA"
+    },
+    {
+        question: "'Your energy is like a stock portfolio: you must know how to invest it, increase it and then share it'",
+        optionA: "Jesus Christ",
+        optionB: "Gustavo Santos",
+        correctOption: "optionB"
+    },
+    {
+        question: "'So I say to you, Ask and it will be given to you; search, and you will find; knock, and the door will be opened for you'",
+        optionA: "Jesus Christ",
+        optionB: "Gustavo Santos",
+        correctOption: "optionA"
+    },
+    {
+        question: "'Freedom is the ultimate exponent of self-love'",
+        optionA: "Jesus Christ",
+        optionB: "Gustavo Santos",
+        correctOption: "optionB"
+    },
+    {
+        question: "'For everyone who exalts himself will be humbled, and everyone who humbles himself will be exalted'",
+        optionA: "Jesus Christ",
+        optionB: "Gustavo Santos",
+        correctOption: "optionA"
+    },
+    {
+        question: "'The truth shakes anyone because we got used to living in the lie'",
+        optionA: "Jesus Christ",
+        optionB: "Gustavo Santos",
+        correctOption: "optionB"
+    },
+    {
+        question: "'Blessed are the merciful, for they will be shown mercy'",
+        optionA: "Jesus Christ",
+        optionB: "Gustavo Santos",
+        correctOption: "optionA"
+    },
+    {
+        question: "'The last wish of a happy person is to be the owner of reason, it doesn't have to, because yours is enough to inspire everyone around them'",
+        optionA: "Jesus Christ",
+        optionB: "Gustavo Santos",
+        correctOption: "optionB"
+    },
+    {
+        question: "'Man shall not live by bread alone, but by every word that proceeds from the mouth of God.'",
+        optionA: "Jesus Christ",
+        optionB: "Gustavo Santos",
+        correctOption: "optionA"
+    },
+    {
+        question: "'The more you complain the less respect you will have'",
+        optionA: "Jesus Christ",
+        optionB: "Gustavo Santos",
+        correctOption: "optionB"
+    },
+    {
+        question: "'If you want to be perfect, go, sell your possessions and give to the poor, and you will have treasure in heaven.'",
+        optionA: "Jesus Christ",
+        optionB: "Gustavo Santos",
+        correctOption: "optionA"
+    },
+    {
+        question: "'Only the truth carries with it the transforming force capable of taking anyone to action'",
+        optionA: "Jesus Christ",
+        optionB: "Gustavo Santos",
+        correctOption: "optionB"
+    },
+    {
+        question: "'For if you love those who love you, what reward have you? Do not even the tax collectors do the same?'",
+        optionA: "Jesus Christ",
+        optionB: "Gustavo Santos",
+        correctOption: "optionA"
+    },
+    {
+        question: "'The weight of unspoken words is heavier and more painful than the cross itself '",
+        optionA: "Jesus Christ",
+        optionB: "Gustavo Santos",
+        correctOption: "optionB"
+    },
+    {
+        question: "'Blessed are the meek, for they will inherit the earth'",
+        optionA: "Jesus Christ",
+        optionB: "Gustavo Santos",
+        correctOption: "optionA"
+    },
+    {
+        question: "'To be like the others I prefer to assume I'm nobody'",
+        optionA: "Jesus Christ",
+        optionB: "Gustavo Santos",
+        correctOption: "optionB"
+    },
+    {
+        question: "'As the Father has loved me, so have I loved you'",
+        optionA: "Jesus Christ",
+        optionB: "Gustavo Santos",
+        correctOption: "optionA"
+    },
+    {
+        question: "'Once happiness is found it needs to be expanded and shared, otherwise it will eventually founder'",
+        optionA: "Jesus Christ",
+        optionB: "Gustavo Santos",
+        correctOption: "optionB"
+    },
+    {
+        question: "'Let the one among you who is without sin be the first to cast a stone'",
+        optionA: "Jesus Christ",
+        optionB: "Gustavo Santos",
+        correctOption: "optionA"
+    },
+    {
+        question: "'Loneliness is not and will never be the absence of others, but the lack you make of yourself'",
+        optionA: "Jesus Christ",
+        optionB: "Gustavo Santos",
+        correctOption: "optionB"
+    },
+    {
+        question: "'Love one another. As I have loved you, so you must love one another'",
+        optionA: "Jesus Christ",
+        optionB: "Gustavo Santos",
+        correctOption: "optionA"
+    },
+    {
+        question: "'There is no resurrection without trust and much less happiness if you don't trust yourself'",
+        optionA: "Jesus Christ",
+        optionB: "Gustavo Santos",
+        correctOption: "optionB"
+    },
+    {
+        question: "'Do not let your hearts be troubled. Trust in God'",
+        optionA: "Jesus Christ",
+        optionB: "Gustavo Santos",
+        correctOption: "optionA"
+    },
+    {
+        question: "'If there are dangerous people, it's people dominated by the ego'",
+        optionA: "Jesus Christ",
+        optionB: "Gustavo Santos",
+        correctOption: "optionB"
+    },
+]
+const button1 = document.getElementById("option_one")
+const button2 = document.getElementById("option_two")
+
+let shuffledQuestions = []
+let questionNumber    = 1
+let playerScore       = 0  
+let wrongAttempt      = 0 
+let indexNumber       = 0
+
+var music1 = new Audio('assets/gustavo1.mp3');
+var music2 = new Audio('assets/jesus1.mp3');
+var music3 = new Audio('assets/gustavo2.mp3');
+var music4 = new Audio('assets/jesus2.mp3');
+
+// Displaying next question in the array to DOM
+function NextQuestion(index) {
+    handleQuestions()
+
+    const currentQuestion = shuffledQuestions[index]
+    
+    document.getElementById("display_question").innerHTML = currentQuestion.question;
+    document.getElementById("option_one").innerHTML = currentQuestion.optionA;
+    document.getElementById("option_two").innerHTML = currentQuestion.optionB;
+}
+
+// Shuffle and push 10 questions to shuffledQuestions array
+function handleQuestions() { 
+    while (shuffledQuestions.length <= 9) {
+        const random = questions[Math.floor(Math.random() * questions.length)]
+        
+        if (!shuffledQuestions.includes(random)) {
+            shuffledQuestions.push(random)
+        }
+    }
+}
+
+function handleNextQuestion(x) {
+    button1.disabled = true
+    button2.disabled = true
+
+    checkAnswer(x)
+    
+    setTimeout(() => {
+        if (indexNumber <= 9) {
+            NextQuestion(indexNumber)
+
+            button1.disabled = false
+            button2.disabled = false
+        }
+        else {
+            handleEndGame()
+            
+            button1.disabled = false
+            button2.disabled = false
+        }
+    }, 4000);
+}
+
+function checkAnswer(x) {
+    const currentQuestion = shuffledQuestions[indexNumber] // Get question 
+    const currentAnswer = currentQuestion.correctOption    // Get answer
+    
+    let option = null
+    
+    if(x === 1) {
+        option = document.getElementById("option_one");
+    }
+    else {
+        option = document.getElementById("option_two");
+    }
+    
+    if(option.value === currentAnswer) {
+        playerScore++
+        indexNumber++
+
+        setTimeout(() => {
+            notShow()
+        }, 3950);
+
+        show(0)
+
+        if(option.value === "optionA") {
+            music4.play();
+
+            setTimeout(() => {
+                music4.pause();
+                music4.currentTime = 0
+            }, 3950);
+        }
+        else {
+            music3.play();
+
+            setTimeout(() => {
+                music3.pause();
+                music4.currentTime = 0
+            }, 3950);
+        }
+    }
+    else if(option.value !== currentAnswer) { 
+        wrongAttempt++
+        indexNumber++
+
+        setTimeout(() => {
+            notShow()
+        }, 3950);
+
+        show(1)
+
+        if(option.value === "optionA") {
+            music2.play();
+
+            setTimeout(() => {
+                music2.pause();
+                music4.currentTime = 0
+            }, 3950);
+        }
+        else {
+            music1.play();
+
+            setTimeout(() => {
+                music1.pause();
+                music4.currentTime = 0
+            }, 3950);
+        }
+    }
+}
+
+// Function for when all questions being answered
+function handleEndGame() {
+    const grade = (playerScore / 10) * 100
+
+    // Data to display to score board
+    document.getElementById('grade_percentage').innerHTML = grade
+    document.getElementById('wrong_answers').innerHTML = wrongAttempt
+    document.getElementById('right_answers').innerHTML = playerScore
+    document.getElementById('score_modal').style.display = "flex"
+}
+
+// Closes score and resets
+function closeScoreModal() {
+    questionNumber    = 1
+    playerScore       = 0
+    wrongAttempt      = 0
+    indexNumber       = 0
+    shuffledQuestions = []
+
+    NextQuestion(indexNumber)
+    document.getElementById('score_modal').style.display = "none";
+}
+
+function notShow() {
+    document.getElementById('noice').style.display = "none"
+    document.getElementById('not_noice').style.display = "none"
+}
+function show(y) {
+    if(y === 0) {
+        document.getElementById('noice').style.display = "flex"
+    }
+    else {
+        document.getElementById('not_noice').style.display = "flex"
+    }
+}
